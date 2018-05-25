b564ab8328106e4be25efab636518b4c2a8f3051
msgirlperl <mspeert@gmail.com>
2018-05-25 12:43:23 -0400

    changing to use this instead of self

--- js/app.js
+++ js/app.js
@@ -12,154 +12,154 @@ var app = {
   canvasMode: null,
   selectedLineIndex: null,
 
-  init: function() {
-    var self = this;
-    if(self.initDone)
+  init: function () {
+  //  var self = this;
+    if(this.initDone)
       return;
-    self.canvasMode = self.modeEnum.DRAW;
-    self.bindToolbarEvents();
-    self.bindDrawAreaEvents();
-    self.initDone = true;
+    this.canvasMode = this.modeEnum.DRAW;
+    this.bindToolbarEvents();
+    this.bindDrawAreaEvents();
+    this.initDone = true;
   },
 
-  bindToolbarEvents: function() {
-    var self = this;
-    document.getElementById('btn-line').addEventListener('click', function() {
-      self.canvasMode = self.modeEnum.DRAW;
-      self.unSelectLines();
-      self.pos = null;
-      self.updateToolbarState();
+  bindToolbarEvents: function () {
+  //  var self = this;
+    document.getElementById('btn-line').addEventListener('click', () => {
+      this.canvasMode = this.modeEnum.DRAW;
+      this.unSelectLines();
+      this.pos = null;
+      this.updateToolbarState();
     });
-    document.getElementById('btn-erase').addEventListener('click', function() {
-      if (self.canvasMode === self.modeEnum.SELECT && self.selectedLineIndex > -1) {
-        self.lines.splice(self.selectedLineIndex, 1);
-        self.selectedLineIndex = -1;
-        self.render();
+    document.getElementById('btn-erase').addEventListener('click', () => {
+      if (this.canvasMode === this.modeEnum.SELECT && this.selectedLineIndex > -1) {
+        this.lines.splice(this.selectedLineIndex, 1);
+        this.selectedLineIndex = -1;
+        this.render();
       } else {
-        self.canvasMode = self.modeEnum.ERASE;
-        self.updateToolbarState();
+        this.canvasMode = this.modeEnum.ERASE;
+        this.updateToolbarState();
       }
-      self.pos = null;
+      this.pos = null;
     });
-    document.getElementById('btn-select').addEventListener('click', function() {
-      self.canvasMode = self.modeEnum.SELECT;
-      self.pos = null;
-      self.updateToolbarState();
+    document.getElementById('btn-select').addEventListener('click', () => {
+      this.canvasMode = this.modeEnum.SELECT;
+      this.pos = null;
+      this.updateToolbarState();
     });
-    document.getElementById('btn-pencil').addEventListener('click', function() {
-      self.canvasMode = self.modeEnum.PENCIL;
-      self.pos = null;
-      self.unSelectLines();
-      self.updateToolbarState();
+    document.getElementById('btn-pencil').addEventListener('click', () => {
+      this.canvasMode = this.modeEnum.PENCIL;
+      this.pos = null;
+      this.unSelectLines();
+      this.updateToolbarState();
     });
-    document.getElementById('btn-move').addEventListener('click', function() {
-      self.canvasMode = self.modeEnum.MOVE;
-      self.unSelectLines();
-      self.pos = null;
-      self.updateToolbarState();
+    document.getElementById('btn-move').addEventListener('click', () => {
+      this.canvasMode = this.modeEnum.MOVE;
+      this.unSelectLines();
+      this.pos = null;
+      this.updateToolbarState();
     });
   },
 
-  unSelectLines: () => {
-    if (self.selectedLineIndex > -1){
-      self.selectedLineIndex = -1;
-      self.render();
+  unSelectLines: function () {
+    if (this.selectedLineIndex > -1){
+      this.selectedLineIndex = -1;
+      this.render();
     }
   },
 
-  isEraseMode: function() {
-    var self = this;
-    return self.canvasMode === self.modeEnum.ERASE ;
+  isEraseMode: function () {
+  //  var self = this;
+    return this.canvasMode === this.modeEnum.ERASE;
   },
-  isDrawMode: function() {
-    var self = this;
-    return self.canvasMode === self.modeEnum.DRAW ;
+  isDrawMode: function () {
+  //  var self = this;
+    return this.canvasMode === this.modeEnum.DRAW;
   },
-  isSelectMode: function() {
-    var self = this;
-    return self.canvasMode === self.modeEnum.SELECT ;
+  isSelectMode: function () {
+  //  var self = this;
+    return this.canvasMode === this.modeEnum.SELECT;
   },
-  isPencilMode: function() {
-    var self = this;
-    return self.canvasMode === self.modeEnum.PENCIL ;
+  isPencilMode: function () {
+  //  var self = this;
+    return this.canvasMode === this.modeEnum.PENCIL;
   },
-  isMoveMode: function() {
-    var self = this;
-    return self.canvasMode === self.modeEnum.MOVE ;
-  },
-
-  updateToolbarState: function() {
-    var self = this;
-    document.getElementById('btn-erase').className = self.isEraseMode() ? 'active' : '';
-    document.getElementById('btn-line').className = self.isDrawMode() ? 'active' : '';
-    document.getElementById('btn-select').className = self.isSelectMode() ? 'active' : '';
-    document.getElementById('btn-pencil').className = self.isPencilMode() ? 'active' : '';
-    document.getElementById('btn-move').className = self.isMoveMode() ? 'active' : '';
+  isMoveMode: function () {
+  //  var self = this;
+    return this.canvasMode === this.modeEnum.MOVE;
   },
 
-  bindDrawAreaEvents: function() {
-    var self = this;
+  updateToolbarState: function () {
+  //  var self = this;
+    document.getElementById('btn-erase').className = this.isEraseMode() ? 'active' : '';
+    document.getElementById('btn-line').className = this.isDrawMode() ? 'active' : '';
+    document.getElementById('btn-select').className = this.isSelectMode() ? 'active' : '';
+    document.getElementById('btn-pencil').className = this.isPencilMode() ? 'active' : '';
+    document.getElementById('btn-move').className = this.isMoveMode() ? 'active' : '';
+  },
+
+  bindDrawAreaEvents: function () {
+  //  var self = this;
     var canvas = document.getElementById('canvas');
-    canvas.addEventListener('click', function(e) {
+    canvas.addEventListener('click', (e) => {
       var x = e.offsetX, y = e.offsetY;
-      if (self.isEraseMode()) {
-        self.eraseLine(x,y);
-      } else if (self.isDrawMode()) {
-          self.drawLine(x,y);
-      } else if (self.isSelectMode()) {
-          self.selectLine(x,y);
+      if (this.isEraseMode()) {
+        this.eraseLine(x,y);
+      } else if (this.isDrawMode()) {
+          this.drawLine(x,y);
+      } else if (this.isSelectMode()) {
+          this.selectLine(x,y);
       }
-      self.render();
+      this.render();
     });
 
-    canvas.addEventListener('mousedown', function(e) {
+    canvas.addEventListener('mousedown', (e) => {
 
-      if (self.isPencilMode() && e.which === 1){
+      if (this.isPencilMode() && e.which === 1){
 
-        self.pos = [ e.offsetX, e.offsetY ];
+        this.pos = [ e.offsetX, e.offsetY ];
 
         let objectLines = []; // will hold the lines created while dragging the mouse
-        self.lines.push(objectLines);
+        this.lines.push(objectLines);
 
           const mouseMoveHandler = (evt) => {
 
             let newX = evt.offsetX, newY = evt.offsetY;
-            let length = Geometry.distance(self.pos[0], self.pos[1], newX, newY);
-            let line = new Line(self.pos[0], self.pos[1], newX, newY, length);
+            let length = Geometry.distance(this.pos[0], this.pos[1], newX, newY);
+            let line = new Line(this.pos[0], this.pos[1], newX, newY, length);
 
             objectLines.push(line);
-            self.render();
+            this.render();
 
-            self.pos = [ newX, newY ];
+            this.pos = [ newX, newY ];
           };
           canvas.addEventListener('mousemove', mouseMoveHandler);
 
           const mouseUpHandler = (evt) => {
-            self.pos = null;
+            this.pos = null;
             canvas.removeEventListener('mousemove', mouseMoveHandler);
             canvas.removeEventListener('mouseup', mouseUpHandler);
           };
 
           canvas.addEventListener('mouseup', mouseUpHandler);
-      } else if (self.isMoveMode() && e.which === 1){
+      } else if (this.isMoveMode() && e.which === 1){
 
-          self.pos = [ e.offsetX, e.offsetY ];
-          self.selectLine(e.offsetX, e.offsetY);
-          if (self.selectedLineIndex > -1){
+          this.pos = [ e.offsetX, e.offsetY ];
+          this.selectLine(e.offsetX, e.offsetY);
+          if (this.selectedLineIndex > -1){
 
             const mouseMoveHandler = (evt) => {
 
               let moveX = evt.offsetX - e.offsetX;
               let moveY = evt.offsetY - e.offsetY;
-              let selectedLines = self.lines[self.selectedLineIndex];
+              let selectedLines = this.lines[this.selectedLineIndex];
               selectedLines.map(line => line.move(moveX, moveY))
-              self.render();
+              this.render();
 
-              self.pos = [ evt.offsetX, evt.offsetY ];
+              this.pos = [ evt.offsetX, evt.offsetY ];
             };
             canvas.addEventListener('mousemove', mouseMoveHandler);
             const mouseUpHandler = (evt) => {
-              self.pos = null;
+              this.pos = null;
               canvas.removeEventListener('mousemove', mouseMoveHandler);
               canvas.removeEventListener('mouseup', mouseUpHandler);
             };
@@ -173,10 +173,10 @@ var app = {
   },
 
   findClosestIndex: function(x,y) {
-    self = this;
-    if (self.lines.length > 0) { // if there's something to select
+  //  self = this;
+    if (this.lines.length > 0) { // if there's something to select
       var minSquareDistance, closestIndex;
-      self.lines.forEach((objectLines, index) => {
+      this.lines.forEach((objectLines, index) => {
         objectLines.forEach((line) => {
           let squareDistance = line.squareDistanceFrom(x, y);
           if((minSquareDistance === undefined) || (squareDistance < minSquareDistance)) {
@@ -191,52 +191,52 @@ var app = {
   },
 
   selectLine: function(x,y) {
-    self = this;
-    let closestIndex = self.findClosestIndex(x, y);
-    let line = self.lines[closestIndex];
+  //  self = this;
+    let closestIndex = this.findClosestIndex(x, y);
+    let line = this.lines[closestIndex];
     //if (line && line.squareDistanceFrom(x,y) <= 100) {// only want lines within 10 pixels
     if (line && line.some(l => l.squareDistanceFrom(x, y) <= 100 )) {
-      self.selectedLineIndex = closestIndex;
+      this.selectedLineIndex = closestIndex;
     } else {
-      self.selectedLineIndex = -1;
+      this.selectedLineIndex = -1;
     }
   },
 
   eraseLine: function(x,y) {
-    self = this;
-    let closestIndex = self.findClosestIndex(x, y);
+  //  self = this;
+    let closestIndex = this.findClosestIndex(x, y);
     if (closestIndex > -1) {
-      self.lines.splice(closestIndex, 1);
+      this.lines.splice(closestIndex, 1);
     }
   },
 
-  drawLine: function(x,y){
-    self = this;
-    if(!self.pos) {
+  drawLine: function(x,y) {
+  //  self = this;
+    if(!this.pos) {
       // save first click of the line
-      self.pos = [ x, y ];
+      this.pos = [ x, y ];
     } else {
       // create the line and add to the list
-      var x0 = self.pos[0], y0 = self.pos[1];
+      var x0 = this.pos[0], y0 = this.pos[1];
       var length = Geometry.distance(x0, y0, x, y);
       var line = new Line(x0, y0, x, y, length);
 
       let objectLine = [ line ];
-      self.lines.push(objectLine);
-      self.pos = null;
+      this.lines.push(objectLine);
+      this.pos = null;
     }
   },
 
   render: function() {
-    var self = this;
+  //  var self = this;
     var canvas = document.getElementById('canvas');
     if (canvas) {
       var ctx = canvas.getContext('2d');
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      self.lines.forEach((objectLines, index) => {
+      this.lines.forEach((objectLines, index) => {
         objectLines.forEach(line => {
           line.draw(ctx);
-          if (index === self.selectedLineIndex) {
+          if (index === this.selectedLineIndex) {
             line.drawEnds(ctx);
           }
         });
@@ -244,3 +244,4 @@ var app = {
     }
   },
 };
+app.init();
